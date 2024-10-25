4bb7ae2f7fe17753ca4f76a381ea70587afbac82
Ciarango10 <carlosarango3101@gmail.com>
2024-10-25 13:51:03 -0500

    Implementacion del header

--- bookmytour-project/src/Components/Header.jsx
+++ bookmytour-project/src/Components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Styles from '../Styles/Header.module.css'
+import { Link } from 'react-router-dom'
+import { routes } from './utils/routes'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header id={Styles.header}>
+      <nav>
+        <ul id={Styles.nav}>
+          <Link to={routes.home} className={Styles.leftSection}>
+            <img id={Styles.logo} src="public/images/logosobreblanco.svg" alt="Logo-LightMode" />
+            <div>
+              <h1 id={Styles.mainTitle}><span id={Styles.blueText}>BOOK</span> MY TOUR</h1>
+              <h2 id={Styles.slogan}>Keep  calm  and  travel on</h2>
+            </div>
+          </Link>
+          <Link className={Styles.rightSection}>
+            <button id={Styles.signUpBtn} className={Styles.headerBtns}>Crear cuenta</button>
+            <button id={Styles.loginBtn} className={Styles.headerBtns}>Iniciar sesión</button>
+          </Link>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
