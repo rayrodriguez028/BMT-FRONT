c1f5254d069209180288cf05a0326ef81afc5be9
Ciarango10 <carlosarango3101@gmail.com>
2024-10-26 17:03:45 -0500

    Seccion de Categorias y recomendaciones

---
+++ bookmytour-project/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styles from '../Styles/Card.module.css'
+
+const Card = ( {title, img, price, description} ) => {
+  return (
+    <div className={Styles.container}>
+        <img src={img} alt={img} />
+        <h4 className={Styles.title}>{title}</h4>
+        <h5>{price}</h5>
+        <p>{description}</p>
+    </div>
+  )
+}
+
+export default Card
