ee087c950c1d1ba34c633b3cbdf7974ff3caddb1
Ciarango10 <carlosarango3101@gmail.com>
2024-10-28 14:51:56 -0500

    Vista de detalles

--- bookmytour-project/src/App.jsx
+++ bookmytour-project/src/App.jsx
@@ -3,6 +3,7 @@ import { routes } from './Components/utils/routes'
 import Layout from './Layouts/Layout'
 import Home from './Routes/Home'
 import Administracion from './Routes/Administracion'
+import Detalle from './Routes/Detalle'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path={routes.home} element={<Layout />}>
           <Route path={routes.home} element={<Home />} />
           <Route path={routes.administracion} element={<Administracion />} />
+          <Route path="detalle/:id" element={<Detalle />} />
           <Route path={routes.notFound} element={<h1>Error 404 - Page not Found</h1>} />
         </Route>
       </Routes>
--- bookmytour-project/src/Components/Card.jsx
+++ bookmytour-project/src/Components/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Styles from '../Styles/Card.module.css'
+import { Link } from 'react-router-dom'
 
-const Card = ({ title, img, price, description }) => {
+const Card = ({ id, title, img, price, description }) => {
   return (
     <div className={Styles.container}>
+      <Link key={id} to={`detalle/${id}`}>
         <img src={img} alt={img} />
-        <h4 className={Styles.title}>{title}</h4>
-        <h5>${price}</h5>
-        <p>{description}</p>
+          <h4 className={Styles.title}>{title}</h4>
+          <h5>$ {price}</h5>
+          <p>{description}</p>
+      </Link>
     </div>
   )
 }
