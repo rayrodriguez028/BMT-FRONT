44987fafe8671a7a6b1e8b196eef9d435a32e8b0
MarjorieAlonso <mayitodraven@gmail.com>
2024-10-28 20:38:55 -0300

    hu6 refinada

--- bookmytour-project/src/Routes/Administracion.jsx
+++ bookmytour-project/src/Routes/Administracion.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+//import React from 'react'
 import Styles from '../Styles/Administration.module.css'
 
 const Administracion = () => {
   return (
     <>
     <div className={Styles.admin}>
-      <h1>Panel de administracion</h1>
+      <h1>¡Bienvenido a tu perfil de administrador!</h1>
       <div className={Styles.button}> 
-      <button id={Styles.btn}>Agregar paquetes</button>
-      <button id={Styles.btn}>Administrar Clientes</button>
-      <button id={Styles.btn}>Administrar Personal </button>
+        <div className={Styles.square}>
+          <h3>Productos </h3>
+        <button id={Styles.btn}>Listar productos</button>
+        </div>
+      <div className={Styles.square}>
+        <h3>Usuarios </h3>
+      <button id={Styles.btn}>Listar usuarios</button>
+      </div>
+      <div className={Styles.square}>
+        <h3>Reservas </h3>
+      <button id={Styles.btn}>Listar reservas  </button>
+      </div>
+      
       </div>
     </div>
-    {/* para dispositivos moviles  */}
+    {/* para dispositivos moviles cd */}
     <div className={Styles.noAvailable}>
     <h2>Contenido no disponible para movil</h2>
   </div>
