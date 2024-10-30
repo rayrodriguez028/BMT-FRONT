b8a161ddd642f9f72689375edd12130dd16de9ea
Lorena <palacios.rios.lorena@gmail.com>
2024-10-30 01:53:45 -0600

    Agregando texto a Home

--- bookmytour-project/src/Routes/Home.jsx
+++ bookmytour-project/src/Routes/Home.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import Styles from '../Styles/Body.module.css'
-import Card from '../Components/Card.jsx'
-import { useContextGlobalStates } from '../Components/utils/global.context.jsx';
+import React, { useEffect, useState } from "react";
+import Styles from "../Styles/Body.module.css";
+import Card from "../Components/Card.jsx";
+import { useContextGlobalStates } from "../Components/utils/global.context.jsx";
 
 const Home = () => {
   const categories = [
@@ -9,11 +9,11 @@ const Home = () => {
     "Paraísos del Caribe",
     "Aventura",
     "Naturaleza Viva",
-    "Aromas y Sabores"
+    "Aromas y Sabores",
   ];
 
   const { state } = useContextGlobalStates();
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [randomTours, setRandomTours] = useState([]);
 
   const getRandomIds = (count) => {
@@ -25,7 +25,7 @@ const Home = () => {
   };
 
   useEffect(() => {
-    const selectedIds = getRandomIds(4); 
+    const selectedIds = getRandomIds(4);
     const filteredTours = state.data.filter((tour) =>
       selectedIds.includes(tour.id)
     );
@@ -34,28 +34,59 @@ const Home = () => {
 
   const handleChange = (e) => {
     setCity(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Buscar:', city);
-  }
+    console.log("Buscar:", city);
+  };
 
   return (
     <div>
-      <div style={{position:'relative'}}>
-        <img id={Styles.mainImage} src='../../public/images/imagen-marca.png' alt='Main-Image'/>
+      <div style={{ position: "relative" }}>
+        <img
+          id={Styles.mainImage}
+          src="../../public/images/imagen-marca.png"
+          alt="Main-Image"
+        />
         <form id={Styles.container} onSubmit={handleSubmit}>
-          <input type="text" value={city} onChange={handleChange} placeholder="Ingresa la ciudad o región" id={Styles.input}/>
-          <button id={Styles.btnSubmit} className={Styles.btnsForm} type='submit'>Buscar</button>
-          <button type='button' id={Styles.btnFilter} className={Styles.btnsForm}>
-            <img id={Styles.filterIcon} src='public/images/filterIcon.svg' alt='Filter-Icon' />
+          <input
+            type="text"
+            value={city}
+            onChange={handleChange}
+            placeholder="Ingresa la ciudad o región"
+            id={Styles.input}
+          />
+          <button
+            id={Styles.btnSubmit}
+            className={Styles.btnsForm}
+            type="submit"
+          >
+            Buscar
+          </button>
+          <button
+            type="button"
+            id={Styles.btnFilter}
+            className={Styles.btnsForm}
+          >
+            <img
+              id={Styles.filterIcon}
+              src="public/images/filterIcon.svg"
+              alt="Filter-Icon"
+            />
           </button>
         </form>
-      </div>   
+      </div>
       <div className={Styles.sectionContainer}>
-        <h2>Toma fotos y compártelas como nunca antes</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+        <h2>Descubre y agenda experiencias únicas en un solo lugar</h2>
+        <p>
+          En <b>BookMyTour</b> nos aseguramos que no compres un viaje, sino una
+          <b> experiencia de vida</b>. Tenemos la convicción de que al viajar
+          creamos las <b>conexiones</b> y los <b>recuerdos</b> más duraderos de
+          nuestra vida. Por eso, ofrecemos tours cuidadosamente diseñados que te
+          llevarán a explorar lo mejor de cada destino, sumergiéndote en
+          culturas locales y <b>aventuras únicas</b>.
+        </p>
       </div>
       <div className={Styles.categories}>
         <h2 className={Styles.subtitles}>Categorías</h2>
@@ -65,14 +96,16 @@ const Home = () => {
           );
 
           // Barajar los tours aleatoriamente
-          const randomTours = [...filteredTours].sort(() => Math.random() - 0.5).slice(0, 2);
+          const randomTours = [...filteredTours]
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 2);
 
           return (
             <div key={category}>
               <h3>{category}</h3>
               <div className={Styles.cardsContainer}>
                 {randomTours.map((tour) => (
-                  <Card 
+                  <Card
                     id={tour.id}
                     key={tour.id}
                     title={tour.nombre}
@@ -90,19 +123,19 @@ const Home = () => {
         <h2 className={Styles.subtitles}>Recomendaciones</h2>
         <div className={Styles.cardsContainer}>
           {randomTours.map((tour) => (
-            <Card 
+            <Card
               id={tour.id}
-              key={tour.id} 
-              title={tour.nombre} 
-              img={tour.imagenes[0]} 
+              key={tour.id}
+              title={tour.nombre}
+              img={tour.imagenes[0]}
               price={tour.precio}
               description={tour.resumen}
             />
           ))}
-          </div>       
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
