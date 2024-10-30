f8269f4db648d6ae3347b863cb5e6b3d28442bda
Lorena <palacios.rios.lorena@gmail.com>
2024-10-29 23:35:43 -0600

    Se a ñadió H3 de cada tour en el detalle

--- bookmytour-project/src/Routes/Detalle.jsx
+++ bookmytour-project/src/Routes/Detalle.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import Styles from '../Styles/Detalle.module.css'
-import { useNavigate, useParams } from 'react-router-dom';
-import { useContextGlobalStates } from '../Components/utils/global.context';
-import Slider from 'react-slick';
+import React, { useEffect, useState } from "react";
+import Styles from "../Styles/Detalle.module.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContextGlobalStates } from "../Components/utils/global.context";
+import Slider from "react-slick";
 
 const Detail = () => {
   const { id } = useParams();
@@ -11,10 +11,10 @@ const Detail = () => {
   const [zoomImage, setZoomImage] = useState(null);
 
   // Filtrar el tour específico según el ID
-  const tour = state.data.find(tour => tour.id === parseInt(id));
+  const tour = state.data.find((tour) => tour.id === parseInt(id));
 
   if (!tour) {
-    return <div style={{height: "100vh"}}>Tour no encontrado</div>; // Mensaje si el tour no se encuentra
+    return <div style={{ height: "100vh" }}>Tour no encontrado</div>; // Mensaje si el tour no se encuentra
   }
 
   // Configuración del carrusel
@@ -28,12 +28,12 @@ const Detail = () => {
     autoplay: true,
     waitForAnimate: false,
     autoplaySpeed: 2000,
-    cssEase: "linear"
+    cssEase: "linear",
   };
 
   return (
     <div className={Styles.mainContainer}>
-      <div style={{marginTop: "120px"}}>
+      <div style={{ marginTop: "120px" }}>
         <button onClick={() => navigate(-1)} className={Styles.btnRegresar}>
           <img src="../../public/images/Arrow left.svg" alt="" />
           Regresar
@@ -44,38 +44,54 @@ const Detail = () => {
         <div className={Styles.mainImage}>
           <img src={tour.imagenes[0]} alt={tour.imagenes[0]} />
           <div className={Styles.littleCard}>
-            <h3>VIAJE A SANTA MARTA</h3>
-            <p>Long-term rental of villas with 
-            a guarantee of conformity with photographs</p>
-            <button>AGENDAR</button>
+            <h3>{tour.nombre}</h3>
+            <p>
+              Long-term rental of villas with a guarantee of conformity with
+              photographs
+            </p>
+            <button>¡Agenda ahora!</button>
           </div>
         </div>
         <div className={Styles.tourContainer}>
           <div className={Styles.tourInfo}>
-            <Slider {...settings}> 
-              {tour.imagenes.map((image, index) => (
-                index != 0 && ( //Omitimos la primera imagen
-                <div key={index} className={Styles.imageWrapper}>
-                  <img 
-                    className={Styles.carouselImage}
-                    src={image} 
-                    alt={`Imagen del tour ${tour.nombre}`} 
-                    onClick={() => setZoomImage(image)}
-                  />
-                </div>
-                )
-              ))}
-            </Slider> 
-            <div style={{display:'flex', alignItems:'center', marginTop: "30px"}}>
+            <Slider {...settings}>
+              {tour.imagenes.map(
+                (image, index) =>
+                  index != 0 && ( //Omitimos la primera imagen
+                    <div key={index} className={Styles.imageWrapper}>
+                      <img
+                        className={Styles.carouselImage}
+                        src={image}
+                        alt={`Imagen del tour ${tour.nombre}`}
+                        onClick={() => setZoomImage(image)}
+                      />
+                    </div>
+                  )
+              )}
+            </Slider>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "30px",
+              }}
+            >
               <h4>{tour.nombre}</h4>
               <h5>$ {tour.precio}</h5>
             </div>
-            <div style={{display:'flex', alignItems:'center', marginTop:"10px", marginBottom: "10px"}}>
-              <span style={{textAlign:'left'}}>{tour.ubicacion}</span>
-              <span style={{textAlign:'center'}}>{tour.duracion}</span>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "10px",
+                marginBottom: "10px",
+              }}
+            >
+              <span style={{ textAlign: "left" }}>{tour.ubicacion}</span>
+              <span style={{ textAlign: "center" }}>{tour.duracion}</span>
             </div>
           </div>
-          <div id={Styles.description} >
+          <div id={Styles.description}>
             <p>{tour.descripcion}</p>
           </div>
         </div>
@@ -85,10 +101,9 @@ const Detail = () => {
             <img src={zoomImage} alt="Zoom" />
           </div>
         )}
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
